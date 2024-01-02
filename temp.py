d05da265e63dc4b8d10ef566af6a9744513fdbc2
Halil İbrahim <79845872+Halil3509@users.noreply.github.com>
2024-01-02 08:53:21 +0300

    deleted tokens

--- temp.py
+++ temp.py
@@ -2,16 +2,16 @@ import requests
 from bardapi.constants import SESSION_HEADERS
 from bardapi import Bard
 
-token = "egjHFD-wgl29I288FzOF_p2nYDcM5Svp1_nQV0SCRmyW9-w-P9AdK6yMVrg1i6hmHvY89Q."
+token = ""
 
 session = requests.Session()
 session.headers = SESSION_HEADERS
 session.cookies.set("__Secure-1PSID", token)
-session.cookies.set("__Secure-1PSIDTS", "sidts-CjIBPVxjSu3Y0TuLhlNk1FD3_Fq0Z4aZMCaKizVgIiuIZqLdu11yVhR1OiM_mIjM7G680hAA")
-session.cookies.set("__Secure-1PSIDCC", "ABTWhQHK8U_zViWx9L9y6pQVNy79JT1r8Mrqqm2vizOv61zdekSpCVr48mrJZLBELLjr9_IpMsCQ")
+session.cookies.set("__Secure-1PSIDTS", "")
+session.cookies.set("__Secure-1PSIDCC", "")
 
 bard = Bard(token=token, session=session)
 
-output =  bard.get_answer("Halsizlik ifade eden 10 cümle yaz")
+output = bard.get_answer("Halsizlik ifade eden 10 cümle yaz")
 
 print(output)
